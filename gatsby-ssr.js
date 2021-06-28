3ad34b1557d16541ed245cd205e49e23527f5ead
Charles Vandevoorde <charles@vandevoorde.me>
2021-06-28 18:59:29 +0200

    another cloudflare token mismatch

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,6 +3,6 @@ const React = require('react')
 exports.onRenderBody = ({ setPostBodyComponents }) => {
     setPostBodyComponents([
         <script defer key='cloudflare-analytics' src='https://static.cloudflareinsights.com/beacon.min.js' 
-            data-cf-beacon='{"token": "eae4cbdf21884ec280e2847cffaba09b"}' />
+            data-cf-beacon='{"token": "dcade5c9e26f4fed98e085016dd7a91d"}' />
     ])
 }
